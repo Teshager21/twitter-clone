f9aa882375ad88abc6d77b957d7827c00b1b3e93
Teshager <teshager8922@gmail.com>
2020-07-10 00:52:19 -0600

    scaffolds tweeets

---
+++ app/helpers/tweeets_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# tweeets helper
+module TweeetsHelper
+end
---
+++ db/migrate/20200710064507_create_tweeets.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# migrations
+class CreateTweeets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tweeets do |t|
+      t.text :tweeet
+
+      t.timestamps
+    end
+  end
+end
